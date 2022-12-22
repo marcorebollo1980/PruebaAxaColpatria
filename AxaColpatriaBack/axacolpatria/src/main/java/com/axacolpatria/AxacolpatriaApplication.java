f3f671ea09e30114e28b3bbf4adf23a635bc3f7c
marcorebollo1980 <marcorebollolopez@gmail.com>
2022-12-22 15:36:05 -0500

    commit one
    Se crea proyecto con la estructura base y paquetes necesarios.

---
+++ AxaColpatriaBack/axacolpatria/src/main/java/com/axacolpatria/AxacolpatriaApplication.java
@@ -0,0 +1,13 @@
+package com.axacolpatria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxacolpatriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxacolpatriaApplication.class, args);
+	}
+
+}
---
+++ AxaColpatriaBack/axacolpatria/src/test/java/com/axacolpatria/AxacolpatriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axacolpatria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxacolpatriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
